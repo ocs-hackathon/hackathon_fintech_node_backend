2e7617ded4f1fd583c24961e516b16046666b409
NahomTM <nahom6297@gmail.com>
2024-10-01 10:45:30 +0300

    loan route modified

--- routes/offer.js
+++ routes/offer.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { getAllOffers, deleteOffer, updateOffer, createOffer, getAllActiveOffers } = require('../controllers/offer');
+const authenticateToken = require('../middleware/authenticate');
 const router = express.Router();
 
-router.post('/createOffer', createOffer);
-router.get('/getOffers', getAllOffers);
-router.delete('/deleteOffer/:id', deleteOffer);
-router.put('/updateOffer/:id', updateOffer);
-router.get('/getAllActiveOffers', getAllActiveOffers)
+router.post('/createOffer', authenticateToken, createOffer);
+router.get('/getOffers', authenticateToken, getAllOffers);
+router.delete('/deleteOffer/:id', authenticateToken, deleteOffer);
+router.put('/updateOffer/:id', authenticateToken, updateOffer);
+router.get('/getAllActiveOffers', authenticateToken, getAllActiveOffers)
 
 module.exports = router;
